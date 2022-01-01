42f8d6bf789924c89784e12918bf06e0b0f0bb4b
Guilherme Turtera <guilherme.turtera@ploomes.com>
2022-01-01 00:38:01 -0300

    refactor: cleaned booking and event

--- src/graphql/resolvers/merge.js
+++ src/graphql/resolvers/merge.js
@@ -1,17 +1,4 @@
-const Event = require('../../models/event');
 const User = require('../../models/user');
-const Booking = require('../../models/booking');
-
-const { dateToString } = require('../../helpers/date');
-
-const transformEvent = event => {
-    return {
-        ...event._doc,
-        _id: event.id,
-        date: dateToString(event._doc.date),
-        creator: getUser.bind(this, event._doc.creator) 
-    }    
-}
 
 const getUser = async userId => {
     try {
@@ -26,28 +13,4 @@ const getUser = async userId => {
     }
 }
 
-const getEvent = async eventId => {
-    try {
-        const event = await Event.findById(eventId);
-        return transformEvent(event);
-    } catch (err) {
-        throw err;
-    }
-}
-
-const getEvents = async eventIds => {
-    try {
-        const events = await Event.find({ _id: { $in: eventIds } });
-        
-        return events.map(event => {  
-            return transformEvent(event);
-        })
-    } catch (err) {
-        throw err;
-    }             
-}
-
 exports.getUser = getUser;
-exports.getEvents = getEvents;
-exports.getEvent = getEvent;
-exports.transformEvent = transformEvent;
