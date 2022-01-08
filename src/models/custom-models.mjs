3e019071751e98662c13cba3bf086180e082570e
Guilherme Turtera <guiturtera@hotmail.com>
2022-01-07 21:48:27 -0300

    fix: now does not create entity when it does not have any fields

--- src/models/custom-models.mjs
+++ src/models/custom-models.mjs
@@ -14,25 +14,27 @@ for (let i = 0; i < entities.length; i++) {
     const entity = entities[i];
     const fields = await Field.find({ entity: entity._id })
     
-    let auxSchema = new Schema();
-    fields.forEach(field => {
-        const fieldObj = field._doc;
+    if (fields.length > 0) {
+        let auxSchema = new Schema({});
+        fields.forEach(field => {
+            const fieldObj = field._doc;
 
-        auxSchema.add({
-            [fieldObj.name]: {
-                type: fieldObj.type,
-                default: fieldObj.default,
-                re: fieldObj.ref
-            }
+            auxSchema.add({
+                [fieldObj.name]: {
+                    type: fieldObj.type,
+                    default: fieldObj.default,
+                    re: fieldObj.ref
+                }
+            })
         })
-    })
-    
-    let createdModel = model(entity._doc.name, auxSchema);
-    const customizationOptions = {};
-    const createdModelTC = composeMongoose(createdModel, customizationOptions);
+        
+        let createdModel = model(entity._doc.name, auxSchema);
+        const customizationOptions = {};
+        const createdModelTC = composeMongoose(createdModel, customizationOptions);
 
-    CustomModels.push(createdModel);
-    CustomModelsTC.push(createdModelTC);
+        CustomModels.push(createdModel);
+        CustomModelsTC.push(createdModelTC);
+    }
 }
 
 export {
